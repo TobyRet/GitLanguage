bdb9ae1670a385eae8e6a067121e625860a63b2f
Toby Retallick <toby.retallick@gmail.com>
2014-06-17 13:00:44 +0100

    Saving work -rspec probs

--- spec/helpers/sign_in_helper.rb
+++ spec/helpers/sign_in_helper.rb
@@ -9,3 +9,9 @@ def sign_in_david
 	fill_in 'Username', with: 'davidmerfield'
 	click_button 'submit'
 end
+
+def sign_in_fakeuser
+	visit '/'
+	fill_in 'Username', with: 'Xcncjffo'
+	click_button 'submit'
+end
